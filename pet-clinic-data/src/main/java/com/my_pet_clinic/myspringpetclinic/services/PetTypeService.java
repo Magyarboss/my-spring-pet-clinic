2f49e635e8e9bcae172b85003b275088bea34760
Magyarboss <matej_liptak@hotmail.com>
2022-08-02 14:59:41 +0200

    Added PetTypeService and PetTypeMapServiceImpl.  Closes #29

---
+++ pet-clinic-data/src/main/java/com/my_pet_clinic/myspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.my_pet_clinic.myspringpetclinic.services;
+
+import com.my_pet_clinic.myspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
