5681f38bbe4ab8a65d35916c962ec5ead13da919
Magyarboss <matej_liptak@hotmail.com>
2022-08-02 14:11:45 +0200

    Added Speciality, added to Vet.  Closes #27

---
+++ pet-clinic-data/src/main/java/com/my_pet_clinic/myspringpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.my_pet_clinic.myspringpetclinic.model;
+
+public class Speciality extends  BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
