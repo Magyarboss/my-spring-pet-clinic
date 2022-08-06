0014df2dcc95429a5ff67f5dd36fc9e600c5720f
Magyarboss <matej_liptak@hotmail.com>
2022-08-06 20:14:16 +0200

    Added Project Lombok.  Closes #51

--- pet-clinic-data/src/main/java/com/my_pet_clinic/myspringpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/my_pet_clinic/myspringpetclinic/model/Owner.java
@@ -1,12 +1,32 @@
 package com.my_pet_clinic.myspringpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, Set<Pet> pets, String address, String city, String telephone) {
+        super(id, firstName, lastName);
+        this.pets = pets;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
+
+
+
+
+
 
     @Column()
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
@@ -20,35 +40,5 @@ public class Owner extends Person{
 
 
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
 
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
--- pet-clinic-web/src/main/java/com/my_pet_clinic/myspringpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/my_pet_clinic/myspringpetclinic/bootstrap/DataLoader.java
@@ -63,6 +63,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("1233124151");
 
+
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
