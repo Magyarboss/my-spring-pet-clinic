e57e02926e091d6140ccaafffbfd7c0b446be670
Magyarboss <matej_liptak@hotmail.com>
2022-08-08 16:04:01 +0200

    Check to Junit 5.  Closes #4

--- pet-clinic-web/src/test/java/com/my_pet_clinic/myspringpetclinic/MySpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/my_pet_clinic/myspringpetclinic/MySpringPetClinicApplicationTests.java
@@ -1,9 +1,12 @@
 package com.my_pet_clinic.myspringpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MySpringPetClinicApplicationTests {
 
